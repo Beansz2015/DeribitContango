ec4473b647827c084149819437cad57eae2484fe
Beansz2015 <straitsettlements@gmail.com>
2025-10-10 22:27:05 +0800

    v1.7a

--- DeribitContango/frmContangoMain.Designer.vb
+++ DeribitContango/frmContangoMain.Designer.vb
@@ -716,7 +716,7 @@ Partial Class frmContangoMain
         Controls.Add(grpConnection)
         Margin = New Padding(4, 5, 4, 5)
         Name = "frmContangoMain"
-        Text = "Deribit Contango Basis Trader - V1.7"
+        Text = "Deribit Contango Basis Trader - V1.7a"
         grpConnection.ResumeLayout(False)
         grpConnection.PerformLayout()
         grpMarket.ResumeLayout(False)
